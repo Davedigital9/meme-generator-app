b51d4cce2285505e73ab381feb9b4242e96d999a
David <151574925+Davedigital9@users.noreply.github.com>
2024-09-29 19:54:45 +0100

    Resolved the distortion of generated images by improving the styling and structure

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,34 +4,49 @@ import memesData from "../memesData.jsx"
 export default function Meme() {
     const [memeImage, setMemeImage] = React.useState("")
 
+    // const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
+    
     function getMemeImage() {
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url) 
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
     }
-
+    
     return (
         <main>
             <div className="form">
-                <div>
-                    <input
-                        type="text"
-                        placeholder="Top text"
-                        className="form--input"
-                    />
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        placeholder="Bottom text"
-                        className="form--input"
-                    />
-                </div>
-                <button className="form--button" onClick={getMemeImage}>
-                    <span>Get a new meme image 🖼</span>
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
                 </button>
             </div>
-            <img src={memeImage} className="meme--image"/>
+            <div className="meme-container">
+                <img src={meme.randomImage} className="meme--image" />
+            </div>
         </main>
     )
 }
