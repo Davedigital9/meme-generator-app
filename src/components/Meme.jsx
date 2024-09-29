3dd65b98fdb518c162ed13b87dabc6ec645af39e
David <151574925+Davedigital9@users.noreply.github.com>
2024-09-29 19:34:11 +0100

    The Meme.jsx was updated such that when the getMemeImage function is called, update the `memeImage` state to be the random chosen image URL.

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,12 +2,12 @@ import React from "react"
 import memesData from "../memesData.jsx"
 
 export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("")
 
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        console.log(url)
+        setMemeImage(memesArray[randomNumber].url) 
     }
 
     return (
@@ -31,6 +31,7 @@ export default function Meme() {
                     <span>Get a new meme image 🖼</span>
                 </button>
             </div>
+            <img src={memeImage} className="meme--image"/>
         </main>
     )
 }
